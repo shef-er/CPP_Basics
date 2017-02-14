7431f4142337fbbb3bf2b0e509e5822fb44fec42
Ernest Shefer <shef-er@ya.ru>
2017-02-14 13:52:07 +0500

    Update lab9.cpp

--- S2/Practice_lab/lab9.cpp
+++ S2/Practice_lab/lab9.cpp
@@ -25,25 +25,58 @@ vector_cos(vector<double> &a, vector<double> &b)
 	return vector_multiply(a, b) / (sqrt(vector_multiply(a, a)) * sqrt(vector_multiply(b, b)));
 }
 
+void
+vector_centralize(vector<double> &vector)
+{
+	int n = 0;
+	double sum = 0;
+	double mid = 0;
+	for (auto& v : vector)
+	{
+		n++;
+		sum += v;
+	}
+	if (n != 0)
+	{
+		mid = sum / n;
+		for (auto& v : vector)
+			v = v - mid;
+	}
+}
+
+void
+vector_normalize(vector<double> &vector)
+{
+	int max = 0;
+	for (auto& v : vector)
+	{
+		int val = abs(v);
+		if (max < val) max = val;
+	}
+
+	if (max != 0)
+		for (auto& v : vector)
+			v = v / max;
+}
+
+void
+vector_prepare(vector<double> &vector) 
+{
+	vector_centralize(vector);
+	vector_normalize(vector);
+}
+
 int
 main()
 {
 	vector<double> a = { 1, 5, 10, 0, 0, 2, 4 };
 	vector<double> b = { 5, 2, 2, 10, 2, 5, 0 };
 
+	vector_prepare(a);
+	vector_prepare(b);
+
 	cout << vector_cos(a, b);
 	cout << endl;
 
 	return 0;
 }
-
-/*
-vector<double> dv_read(const string &file_name)
-{
-vector<double> result;
-ifstream file(file_name);
-for (double val; file >> val;)
-result.push_back(val);
-return result;
-}
-*/
