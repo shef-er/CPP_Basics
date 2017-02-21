3f16a7307a9f156453b10db2bac1458c9b5b1a55
Ernest Shefer <shef-er@ya.ru>
2017-02-21 12:49:41 +0500

    Update work9.cpp

--- S2/Practice_independent/work9.cpp
+++ S2/Practice_independent/work9.cpp
@@ -10,41 +10,59 @@
 #include <iostream>
 using namespace std;
 
+template <class T>
 void
-compute_middle_4k(vector<double>& input, vector<double>& output, size_t chunk = 2)
+compute_middle_4k(vector<T>& input, vector<T>& output, size_t chunk = 2)
 {
-  size_t out_size = input.size() - chunk + 1;
-  output.clear();
-  for (size_t i = 0; i < out_size; i++)
-  {
-    double o = 0;
-    size_t ii = i + chunk;
-    for (size_t j = i; j < ii; j++)
-      o += input[j];
-    output.push_back(o / chunk);
-  }
+	size_t out_size = input.size() - chunk + 1;
+	output.clear();
+	for (size_t i = 0; i < out_size; i++)
+	{
+		T o = 0;
+		size_t ii = i + chunk;
+		for (size_t j = i; j < ii; j++)
+			o += input[j];
+		output.push_back(o / chunk);
+	}
 
-  for (size_t i = 0; i < out_size; i++)
-  {
-    size_t ii = i + chunk;
-    printf("%f = ", output[i]);
-    cout << "(";
-    for (size_t j = i; j < ii; j++)
-      printf("%f ", input[j]);
-    cout << ")" << endl;
-  }
+	for (size_t i = 0; i < out_size; i++)
+	{
+		size_t ii = i + chunk;
+		cout << output[i] << " = ( ";
+		for (size_t j = i; j < ii; j++)
+			cout << input[j] << " ";
+		cout << ")" << endl;
+	}
+	cout << endl;
+}
 
+int
+test() {
+	vector<double> a, b;
+	vector<float> c, d;
+
+	for (size_t i = 1; i < 10; i++)
+	{
+		a.push_back(i);
+		c.push_back(i);
+	}
+
+	compute_middle_4k(a, b);
+	for (size_t i = 0; i < b.size(); i++)
+	if (b[i] != 0.5 + i)
+		return 1;
+
+	compute_middle_4k(c, d);
+	for (size_t i = 0; i < d.size(); i++)
+	if (d[i] != 0.5 + i)
+		return 2;
+
+	return 0;
 }
 
 int
 main()
 {
-  vector<double> a, b;
-
-  for (size_t i = 0; i < 10; i++)
-    a.push_back(i);
-
-  compute_middle_for_k(a, b, );
-
-  return EXIT_SUCCESS;
+	cout << "Test: " << test() << endl;
+	return EXIT_SUCCESS;
 }
