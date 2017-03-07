b315e91703604004fa56577837c9f4851ecf6897
Ernest Shefer <shef-er@ya.ru>
2017-03-07 13:36:54 +0500

    Update work10.cpp

--- S2/Practice_independent/work10.cpp
+++ S2/Practice_independent/work10.cpp
@@ -5,19 +5,114 @@
 //    2|aii| > Σj |aij| для всех индексов i.]
 //
 //----------------------------------------------------------------------------
-#include <stdlib.h>
-#include <cstdlib>
-#include <cmath>
+#include <vector>
+#include <string>
+#include <fstream>
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
+template <class T>
+vector<vector<T>>
+read_matrix(const string &file_name, size_t X = 3, size_t Y = 3)
+{
+	vector<vector<T>> result(Y, vector<T>(X, 0));
+	T val;
+	ifstream file(file_name);
+
+	for (size_t j = 0; j < Y; j++)
+	{
+		for (size_t i = 0; i < X; i++)
+		{
+			file >> val;
+			cout << val << " ";
+			result[j][i] = val;
+		}
+		cout << endl;
+	}
+	return result;
+}
+
+template <class T>
+bool
+matrix_diagonal_domination(const vector<vector<T>> &input)
+{
+
+	if (input.empty() || input[0].empty())
+		return 0;
+
+	size_t X = input.size();
+	size_t Y = input[0].size();
+
+	for (size_t i = 0; i < X; i++)
+	{
+		if (Y > input[i].size())
+			Y = input[i].size();
+	}
+
+	cout << endl;
+	for (size_t i = 0; i < X; i++)
+	{
+		for (size_t j = 0; j < Y; j++)
+		{
+			cout << input[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	cout << endl;
+	for (size_t i = 0; i < X; i++)
+	{
+		cout << input[i][i] << " * 2 > ";
+		T sum = 0;
+		for (size_t j = 0; j < Y; j++)
+		{
+			cout << abs(input[i][j]) << " ";
+			sum += abs(input[i][j]);
+		}
+		cout << endl;
+		cout << endl;
+		if (input[i][i] * 2 <= sum)
+			return false;
+	}
+
+	return true;
+}
+
+
+int
+test() {
+	vector<vector<double>> matrix1(read_matrix<double>("matrix1.txt"));
+	vector<vector<double>> matrix2(read_matrix<double>("matrix2.txt"));
+
+	cout << endl << "tests!" << endl;
+
+	cout << endl;
+	if (matrix_diagonal_domination(matrix1))
+		cout << "matrix1 is diagonal dominated" << endl;
+	else
+	{
+		cout << "matrix1 is not diagonal dominated" << endl;
+		return 1;
+	}
+
+	cout << endl;
+	if (matrix_diagonal_domination(matrix2))
+		cout << "matrix2 is diagonal dominated" << endl;
+	else
+	{
+		cout << "matrix2 is not diagonal dominated" << endl;
+		return 2;
+	}
+
+	return 0;
+}
 
 int
 main()
 {
- 
+	test();
 
-
-
-  return EXIT_SUCCESS;
+	return 0;
 }
