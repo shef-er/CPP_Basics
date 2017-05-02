791b51a2aefe2d7ffd432ab3258f5636dda646ad
Ernest Shefer <shef-er@ya.ru>
2017-05-02 14:26:34 +0500

    Update work15.cpp

--- S2/Practice_independent/work15.cpp
+++ S2/Practice_independent/work15.cpp
@@ -1,13 +1,12 @@
 /*
-  Пусть дана последовательность некоторых значений (произвольного типа) A,
-    элементы которой можно сравнивать с помощью двух разных критериев
-    качества — компараторов c1 и c2 (параметры).
-  Доминируемым элементом называется такой 
-    a ∈ A, что ∃b ∈ A (¬c1(b, a) ∧ c2(a, b)) ∨ (¬c2(b, a) ∧ c1(a, b)).
-  Т.е. по одному критерию b, по крайней мере, не хуже a,
-    а по другому — строго лучше.
-  Требуется удалить из A все доминируемые элементы за O(|A| log |A|)-время.
-
+Пусть дана последовательность некоторых значений (произвольного типа) A,
+элементы которой можно сравнивать с помощью двух разных критериев
+качества — компараторов c1 и c2 (параметры).
+Доминируемым элементом называется такой
+a ∈ A, что ∃b ∈ A (¬c1(b, a) ∧ c2(a, b)) ∨ (¬c2(b, a) ∧ c1(a, b)).
+Т.е. по одному критерию b, по крайней мере, не хуже a,
+а по другому — строго лучше.
+Требуется удалить из A все доминируемые элементы за O(|A| log |A|)-время.
 */
 #include <numeric>
 #include <iostream>
@@ -17,43 +16,64 @@
 #include <algorithm>
 using namespace std;
 
-template <class T, class C>
-bool
-extrathicc(vector<T>& A, C c1, C c2)
+
+template <class T, class Comp1, class Comp2>
+void
+extrathicc(T& A, Comp1 c1, Comp2 c2)
 {
-  /* ---- before ---- */
-  cout << "before = { ";
-  for (auto a : A)
-    cout << a << " ";
-  cout << "}" << endl;
-  /* ----        ---- */
-  
-  sort(A.begin(), A.end());
 
-  /* ---- after ---- */
-  cout << endl;
-  cout << "after  = { ";
-  for (auto a : A)
-    cout << a << " ";
-  cout << "}" << endl;
-  /* ----       ---- */
+	sort(A.begin(), A.end(), c1);
+	reverse(A.begin(), A.end());
 
-  return false;
+	auto Nend = unique(A.begin(), A.end(),
+		[c2](const pair<int, int> &left, const pair<int, int> &right) {
+			return !(c2(left, right));
+		}
+	);
+
+	A.erase(Nend, A.end());
+
 }
 
 
-int 
+int
 test()
 {
-  vector<int> A = {0, 5, 4, 5, 2, 1, 0};
+	vector<pair<int, int>> A = { 
+		{ 1, 1 }, 
+		{ 2, 2 }, 
+		{ 3, 1 }, 
+		{ 6, 1 }, 
+		{ 2, 3 }, 
+		{ 2, 5 },
+		{ 3, 4 }, 
+		{ 3, 1 },
+	};
 
-  cout << extrathicc(A, less<int>(), less_equal<int>()) << endl;
-  return 0;
+	extrathicc(A, 
+		[](const pair<int, int> &left, const pair<int, int> &right) {
+			return left.first < right.first;
+		},
+		[](const pair<int, int> &left, const pair<int, int> &right) {
+			return left.second < right.second;
+		}
+		);
+
+
+		/* ---- unique ---- */
+		cout << endl;
+		cout << "unique { ";
+		for (auto a : A)
+			cout << " (" << a.first << ", " << a.second << ")";
+		cout << "}" << endl;
+		/* ----       ---- */
+
+	return 0;
 }
 
-int 
+int
 main()
 {
-  test();
-  return 0;
+	test();
+	return 0;
 }
